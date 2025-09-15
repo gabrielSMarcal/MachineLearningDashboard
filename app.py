f5da08a24a4bf141ad44bf2893ae0c7dd7e5d694
gabrielSMarcal <gabriel.santos.marcal1@gmail.com>
2025-09-15 17:29:10 -0300

    Refactor: Enhance navigation bar with Bootstrap components and update external stylesheet to FLATLY

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
 from dash import Dash, dcc, html
 from dash.dependencies import Input, Output
+import dash_bootstrap_components as dbc
 
-app = Dash(__name__, external_stylesheets=['assets/main.css'])
+app = Dash(__name__, external_stylesheets=['assets/main.css', dbc.themes.FLATLY])
+
+navegacao = dbc.NavbarSimple(
+    children=[
+        dbc.NavItem(dbc.NavLink("Gráficos", href="/graficos")),
+        dbc.NavItem(dbc.NavLink("Formulário", href="/formulario")),
+        dbc.NavItem(dbc.NavLink("Início", href="/")),
+    ],
+    brand="Dashboard de Análise de Doença Cardíaca",
+    brand_href="/",
+    color="primary",
+    dark=True,
+)
 
 app.layout = html.Div([
     dcc.Location(id='url', refresh=False),
-    html.Nav([
-        dcc.Link('Gráficos', href='/graficos'),
-        dcc.Link('Formulário', href='/formulario')
-    ]),
+    navegacao,
     html.Div(id='conteudo')
 ])
 
